16d345459915246e50bcddc3712f5590f41b27fc
Nico Hämäläinen <nico@sizeof.io>
2015-09-22 20:24:42 +0300

    Updated test function for purposes

--- test/index.js
+++ test/index.js
@@ -3,6 +3,6 @@ import brawl from '../lib';
 
 describe('brawl', function () {
   it('should have unit test!', function () {
-    assert(brawl() === 'test', 'we expected this package author to add actual unit tests.');
+    assert(brawl("test") === 'Hello test', 'we expected this package author to add actual unit tests.');
   });
 });
